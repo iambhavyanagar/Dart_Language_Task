eec87aaf96039c5eba6ad7d5b5821dc45f55ac93
Bhavya Nagar <153913406+iambhavyanagar@users.noreply.github.com>
2024-05-14 14:18:52 +0530

    Add files via upload

---
+++ Dart_Language_Task/IntroductionToDart/printName.dart
@@ -0,0 +1,6 @@
+// Write a program to print your name in Dart.
+
+void main() {
+  var myName = "Bhavya Nagar";
+  print(myName);
+}
---
+++ Dart_Language_Task/IntroductionToDart/singleDoubleQuotes.dart
@@ -0,0 +1,10 @@
+// Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+
+void main() {
+  String name = "John Doe";
+
+  
+  print("Hello I am \"$name\"");
+
+  print('Hello I\'am "$name"');
+}
