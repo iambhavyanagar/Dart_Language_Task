2888aa363f35b33f77387bafc85a43e79c8456c0
Bhavya Nagar <153913406+iambhavyanagar@users.noreply.github.com>
2024-06-17 09:40:27 +0530

    Add files via upload

--- FundamentalsOfDart/Pr.3 OOP(Architecture)/sum_list_named.dart
+++ FundamentalsOfDart/Pr.3 OOP(Architecture)/sum_list_named.dart
@@ -1,21 +1,19 @@
 import 'dart:io';
-void main()
-{
+
+void main() {
   print("Enter the number of elements:- ");
   int n = int.parse(stdin.readLineSync()!);
   List<int> list = [];
-  for(int i = 0; i < n; i++)
-  {
-    print("Enter element ${i+1}:- ");
+  for (int i = 0; i < n; i++) {
+    print("Enter element ${i + 1}:- ");
     list.add(int.parse(stdin.readLineSync()!));
   }
- print("Sum of all elements is:- ${sumOfListElements}");
+  print("Sum of all elements is:- ${sumOfListElements}");
 }
-int sumOfListElements({required List<int> list})
-{
+
+int sumOfListElements({required List<int> list}) {
   int sum = 0;
-  for(int i = 0; i < list.length; i++)
-  {
+  for (int i = 0; i < list.length; i++) {
     sum = sum + list[i];
   }
   return sum;
